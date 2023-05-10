e3da705aa353b7e958887de6c3532b58851423c9
ZenShibata <zen@ayato.life>
2023-05-10 14:37:17 +0700

    fix: enable ssr

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,6 @@
  * load layout from js file to enable
  * server-side rendering
  */
-export const ssr = false
-export const prerender = true
+export const ssr = true
+export const prerender = false
 export const trailingSlash = 'always'
